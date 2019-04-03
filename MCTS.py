958e5ab8b8cea8047d5e70ba09f120e0dade5f22
steve <stefano.rosa@gmail.com>
2019-04-03 21:24:07 +0200

    optimized mcts, test speed

--- MCTS.py
+++ MCTS.py
@@ -1,22 +1,54 @@
 import math
 import numpy as np
 import random
+import collections
 from collections import namedtuple
 import matplotlib.pyplot as plt
 num_rollouts=100
 type_estimate='avg_value'
 max_iterations=1000000
-
+max_childs=4
 class UCTNode():
-    def __init__(self, board, parent=None, prior=0):
+    def __init__(self, board, move, parent=None):
         self.board = board
+        self.move=move
         self.is_expanded = False
         self.parent = parent  # Optional[UCTNode]
         self.children = {}  # Dict[move, UCTNode]
-        self.prior = prior  # float
-        self.total_value = 0  # float
-        self.number_visits = 0  # int
+        #self.prior = prior  # float
+        #self.total_value = 0  # float
+        #self.number_visits = 0  # int
+        self.child_priors = np.zeros([max_childs], dtype=np.float32)
+        self.child_total_value = np.zeros([max_childs], dtype=np.float32)
+        self.child_number_visits = np.zeros([max_childs], dtype=np.float32)
 
+    @property
+    def number_visits(self):
+        return self.parent.child_number_visits[self.move]
+
+    @number_visits.setter
+    def number_visits(self, value):
+        self.parent.child_number_visits[self.move] = value
+
+    @property
+    def total_value(self):
+        return self.parent.child_total_value[self.move]
+
+    @total_value.setter
+    def total_value(self, value):
+        self.parent.child_total_value[self.move] = value
+
+    def child_Q(self):
+        return self.child_total_value / (1 + self.child_number_visits)
+
+    def child_U(self):
+        return math.sqrt(self.number_visits) * (
+            self.child_priors / (1 + self.child_number_visits))
+
+    def best_child(self):
+        print('node',str(self)[-14:],'childs values', self.child_Q() + self.child_U())
+        return np.argmin(self.child_Q() + self.child_U())
+    '''
     def Q(self):  # returns float
         return self.total_value / (1 + self.number_visits)
 
@@ -27,22 +59,27 @@ class UCTNode():
     #During select_leaf, return the best child node of a given node.
     def best_child(self):
         return min(self.children.values(), key=lambda node: node.Q() + node.U())
-
+    '''
     #Return which node to expand
     def select_leaf(self):
         current = self
         while current.is_expanded:
-            current = current.best_child()
+            best_move_idx = current.best_child()
+            #convert idx move to move
+            print('available moves', [mov for mov in current.board.possible_moves])
+            best_move=[mov for mov in current.board.possible_moves][best_move_idx]
+            current=current.maybe_add_child(best_move, best_move_idx)
         return current
 
     #Called to expand the current node insterting child nodes
-    def expand(self, child_priors, childs):
+    def expand(self, child_priors):
         self.is_expanded = True
-        for move, prior in enumerate(child_priors):
-            self.add_child(move, childs[move], prior)
+        self.child_priors=child_priors
 
-    def add_child(self, move, child, prior):
-        self.children[move] = UCTNode(child, parent=self, prior=prior)
+    def maybe_add_child(self, move, best_move_idx):
+        if best_move_idx not in self.children:
+            self.children[best_move_idx] = UCTNode(self.board.move(move), best_move_idx, parent=self)
+        return self.children[best_move_idx]
 
     def backup(self, value_estimate):
         current = self
@@ -50,14 +87,52 @@ class UCTNode():
             current.number_visits += 1
             current.total_value += value_estimate
             current = current.parent
+#required for the root node. IN order to not 
+#throw an error when the leaf node is searched
+#in def number_visits(self): return self.parent.child_number_visits[self.move]
+class DummyNode(object):
+  def __init__(self):
+    self.parent = None
+    self.child_total_value = collections.defaultdict(float)
+    self.child_number_visits = collections.defaultdict(float)
+
 def search(board):
-    root = UCTNode(board)
-    num_nodes=0
+    root = UCTNode(board, move=None, parent=DummyNode())
+    num_nodes=0 #store the number of nodes explored
     for ite in range(max_iterations+1):#while True:
         num_nodes+=1
-        print('nodes expanded', num_nodes)
+        print('\nnodes expanded', num_nodes)
+        print('root node obj:',str(root)[-14:])
         leaf = root.select_leaf()
+        '''
+        the problem is that i defined the best action as the one with min value
+        the actions are always 4 (even if sometimes 4 actions are not available but only 1/2/3)
+        however the value of the 4th action is still in the array.
+        this value is 0.
+        So, it means that it is the best value possible which is wrong. It must be the worst value
+
+        2 solutions:
+            use max but I have to revert the score becvause at the moment the best score is the lower, instead should
+            be the higher one.
+
+            set the priors to 1 instead to 0 when created the array of priors.
+        
+        let's try to set the priors value as 1 as default.
+        one is not enough. at the moment the solution is to use inf but is ugly 
+        TODO:
+        test if setting as infinite affects the speed of the MCTS.
+        if so:
+            implement the first solution
+        otherwise:
+            keep it
+        '''
+        print('exploring leaf node', leaf)
+        print('lead node board:\n',leaf.board)
+
+
+        #if solved of reached limit explored nodes
         if leaf.board.solved or ite==max_iterations:
+            print('number node explored', num_nodes)
             #obtain the depth of the selected node
             depth=0
             buf_leaf=leaf
@@ -66,7 +141,8 @@ def search(board):
                 buf_leaf=buf_leaf.parent
             #return the result
             Result = namedtuple('Result', 'board, depth, nodesExpanded, max_depth')
-            print('<<ATTENTION>> Max depth is not the real one, it uses the solved node depth')
+            print('<<ATTENTION>> Max depth is not the real one, it uses the solved node depth.\
+                to compute the depth add an attribute to each node that get incremented when it is expanded the first time')
             return Result(leaf.board, depth, num_nodes, depth)
 
         #compute probability to select each child node. (They are all equal probable)
@@ -89,8 +165,12 @@ def search(board):
                 values.append(buff_board.score)
             #the estimate is the avg of the score of 100 successive expansions of the node
             value_estimate=np.mean(values)
-        
+        #convert list to array and pad to match max num of childs
+        #child_priors_np=np.ones(max_childs)
+        child_priors_np=np.full(max_childs, np.inf) #create array of inf values so that the impossible moves are never selected
+        child_priors_np[:len(child_priors)]=child_priors
+
         #expand the leaf node 
-        leaf.expand(child_priors, childs)
+        leaf.expand(child_priors_np)
         #update the value of all the parent nodes
         leaf.backup(value_estimate)
